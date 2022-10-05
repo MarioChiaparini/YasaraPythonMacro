edf28393f3ff7a63266679dff7a1c3e7583cee21
Mario_Chiaparini <45772937+MarioChiaparini@users.noreply.github.com>
2022-10-05 10:53:39 -0300

    Add files via upload

--- yapycon/sending2cavity.py
+++ yapycon/sending2cavity.py
@@ -11,10 +11,11 @@ objects = JoinObj(2,1)
 def Mol2Rec(objects, atomref):
     center1 = GroupCenter(f"{objects[0]} 1 Mol A", coordsys="global", Type="Geometric")
     center2 = GroupCenter(f"{objects[1]} 2 Mol A", coordsys="global", Type="Geometric")
+    
     localone = PosAtom(selection1=f"{objects[0]} Mol A {atomref}", coordsys="local")
     globalone = PosAtom(selection1=f"{objects[0]} Mol A {atomref}", coordsys="global")
 
-    return MoveObj(selection1=f"{objects[1]}", x=localone[0], y=localone[1], z=localone[2])
+    return MoveObj(selection1=f"{objects[1]}", x=globalone[0], y=globalone[1], z=globalone[2])
 
 #get cavities 
 def CaviScene(PATH):
@@ -23,5 +24,7 @@ def CaviScene(PATH):
     return ShowCaviAll(Type="Molecular")
 
 #Docking simulation
-def DockingExec():
-    None
+def DockingExec(converge, ligand, receptor, methods):
+    ExperimentMinimization(convergence=f"{converge}")
+    return ExperimentDocking(method=f"{methods}", ligandobj=f"{ligand}", receptorobj=f"{receptor}", 
+                             runs=None)
